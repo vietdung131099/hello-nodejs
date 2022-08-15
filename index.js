391501d6bc08ce724aa029b0419a61d2d86acccd
nhaiben_company <vietdung131099@gmail.com>
2022-08-15 09:35:54 +0700

    init project express

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.get('/contact', function (req, res) {
+    res.send('Hello node');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
